74935e709b5588972db8f6ee8c200d0cb9c5956e
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-11-06 17:46:17 +0100

    move sub-recipes to be included properly

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,10 @@
 # Recipe:: default
 #
 
-include_recipe "nginx"
 include_recipe "dop_nginx::ngx_pagespeed"
 include_recipe "dop_nginx::naxsi"
 include_recipe "dop_nginx::geoip"
+include_recipe "nginx"
 
 directory "#{node['nginx']['dir']}/ssl" do
   owner "root"
