bcc420b1b49db61941bd62be34250cefdebc29f2
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-11-18 11:50:23 +0100

    upgrade & fix ngx_pagespeed

--- recipes/ngx_pagespeed.rb
+++ recipes/ngx_pagespeed.rb
@@ -25,7 +25,7 @@ mount node['nginx']['ngx_pagespeed']['FileCachePath'] do
 end
 
 ngx_pagespeed_src_filename = ::File.basename(node['nginx']['ngx_pagespeed']['url'])
-ngx_pagespeed_src_filepath = "#{Chef::Config['file_cache_path']}/#{ngx_pagespeed_src_filename}"
+ngx_pagespeed_src_filepath = "#{Chef::Config['file_cache_path']}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"
 ngx_pagespeed_extract_path = "#{Chef::Config['file_cache_path']}"
 
 remote_file ngx_pagespeed_src_filepath do
@@ -48,13 +48,13 @@ bash "extract_ngx_pagespeed" do
   cwd ::File.dirname(ngx_pagespeed_src_filepath)
   code <<-EOH
     mkdir -p #{ngx_pagespeed_extract_path}
-    tar xzf #{ngx_pagespeed_src_filename} -C #{ngx_pagespeed_extract_path}
+    tar xzf ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']} -C #{ngx_pagespeed_extract_path}
   EOH
 end
 
 ngx_psol_src_filename = ::File.basename(node['nginx']['ngx_pagespeed']['psol']['url'])
 ngx_psol_src_filepath = "#{Chef::Config['file_cache_path']}/#{ngx_psol_src_filename}"
-ngx_psol_extract_path = "#{ngx_pagespeed_extract_path}/#{ngx_pagespeed_src_filename}"
+ngx_psol_extract_path = "#{ngx_pagespeed_extract_path}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"
 
 remote_file ngx_psol_src_filepath do
   source node['nginx']['ngx_pagespeed']['psol']['url']
@@ -91,4 +91,4 @@ template "#{node['nginx']['dir']}/conf.d/ngx_pagespeed.conf" do
 end
 
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--add-module=#{Chef::Config['file_cache_path']}/#{ngx_pagespeed_src_filename}"]
+  node.run_state['nginx_configure_flags'] | ["--add-module=#{Chef::Config['file_cache_path']}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"]
