dcc7877d5b63e9b5d463f4c93d4993acffa18be1
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-03-21 23:19:46 +0100

    [ngx_pagespeed] fix download url to prevent redirect limits; [ngx_pagespeed] configure CacheFlushFilename [ngx_pagespeed] enable extend_cache by default

--- attributes/ngx_pagespeed.rb
+++ attributes/ngx_pagespeed.rb
@@ -2,7 +2,7 @@
 # ngx_pagespeed #
 # # # # # # # # #
 default['nginx']['ngx_pagespeed']['version'] = "1.7.30.4-beta"
-default['nginx']['ngx_pagespeed']['url'] = "https://github.com/pagespeed/ngx_pagespeed/archive/v#{node['nginx']['ngx_pagespeed']['version']}.tar.gz"
+default['nginx']['ngx_pagespeed']['url'] = "https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v#{node['nginx']['ngx_pagespeed']['version']}"
 default['nginx']['ngx_pagespeed']['checksum'] = "79403dac41701652410d1632d76a41bfcd5a6321047abfd0e9b82de6f7f83d84e7236ec99bee39d663fc3ee147048777b7b9ac54dc96d1f14d1391d5f20c5002"
 
 default['nginx']['ngx_pagespeed']['psol']['version'] = "1.7.30.4"
@@ -13,5 +13,6 @@ default['nginx']['ngx_pagespeed']['cache-url'] = "/var/ngx_pagespeed_cache"
 default['nginx']['ngx_pagespeed']['cache-size'] = "128M"
 default['nginx']['ngx_pagespeed']['enabled'] = "on"
 default['nginx']['ngx_pagespeed']['FileCachePath'] = node['nginx']['ngx_pagespeed']['cache-url']
-default['nginx']['ngx_pagespeed']['EnableFilters'] = "collapse_whitespace,combine_css,combine_javascript,remove_comments,sprite_images"
+default['nginx']['ngx_pagespeed']['CacheFlushFilename'] = "cache.flush"
+default['nginx']['ngx_pagespeed']['EnableFilters'] = "collapse_whitespace,combine_css,combine_javascript,remove_comments,sprite_images,extend_cache"
 default['nginx']['ngx_pagespeed']['DisableFilters'] = nil
